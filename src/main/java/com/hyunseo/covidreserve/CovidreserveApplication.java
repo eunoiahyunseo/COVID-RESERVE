662f228d56f4f94c6c15bc9819409e6deb78d5d3
eunoiahyunseo <heart20021010@gmail.com>
2023-01-05 09:57:29 +0900

    INIT: initial init
    spring-web-mvc project code-reserve

---
+++ src/main/java/com/hyunseo/covidreserve/CovidreserveApplication.java
@@ -0,0 +1,13 @@
+package com.hyunseo.covidreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidreserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyunseo/covidreserve/CovidreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunseo.covidreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a0744b6205ca462d1e7db2cedecc2078c789a7c3
eunoiahyunseo <heart20021010@gmail.com>
2023-01-05 21:44:26 +0900

    Update #11 - admin view controllers and views
    어드민 뷰 메소드에 인자 입력 및 모델 전달 코드 추가
    관찰 가능하게끔 일부 뷰에 타임리프 코드 추가

--- src/main/java/com/hyunseo/covidreserve/controller/EventController.java
+++ src/main/java/com/hyunseo/covidreserve/controller/EventController.java
@@ -1,9 +1,15 @@
 package com.hyunseo.covidreserve.controller;
 
+import com.hyunseo.covidreserve.constant.EventStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ihyeonseo
@@ -14,8 +20,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EventController {
 
     @GetMapping("/")
-    public String events() {
-        return "event/index";
+    public ModelAndView events(
+            Integer placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime
+    ) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeName", "place-" + placeId);
+        map.put("eventName", eventName);
+        map.put("eventStatus", eventStatus);
+        map.put("eventStartDatetime", eventStartDatetime);
+        map.put("eventEndDatetime", eventEndDatetime);
+
+        return new ModelAndView("admin/events", map);
     }
 
     @GetMapping("/{eventId}")

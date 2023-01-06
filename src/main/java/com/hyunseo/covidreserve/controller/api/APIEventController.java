489ff4ac131a2ee8b84ae7652cfcad15ed4dbe1d
eunoiahyunseo <heart20021010@gmail.com>
2023-01-06 17:33:15 +0900

    Change #11 - ResponseEntityExceptionHandler
    스프링 Web MVC에서 처리해주는 Exception들을 처리해주기 위해
    APIExceptionhandler에 ResponseEntityExceptionHandler을 상속받아서 
    handleExceptionInternal을 Overriding했음

--- src/main/java/com/hyunseo/covidreserve/controller/api/APIEventController.java
+++ src/main/java/com/hyunseo/covidreserve/controller/api/APIEventController.java
@@ -1,5 +1,7 @@
 package com.hyunseo.covidreserve.controller.api;
 
+import com.hyunseo.covidreserve.exception.GeneralException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,21 +14,22 @@ import java.util.List;
 public class APIEventController {
 
     @GetMapping("/events")
-    public List<String> getEvents() {
-        //throw new GeneralException("테스트 에러");
-        return List.of("event1", "event2");
+    public List<String> getEvents() throws Exception {
+        throw new HttpRequestMethodNotSupportedException("GET");
+        // return List.of("event1", "event2");
     }
 
     // 우리가 지정한 error가 아니므로, 공통 Error page로 넘어가게 됩니다.
     @PostMapping("/events")
-    public Boolean createEvent() {
-        // throw new RuntimeException("run-time 테스트 에러");
-        return true;
+    public Boolean createEvent() { 
+        throw new GeneralException("장군님");
+        // return true;
     }
 
     @GetMapping("/events/{eventId}")
     public String getEvent(@PathVariable Integer eventId) {
-        return "event " + eventId;
+        throw new RuntimeException("런타임 에러");
+        // return "event " + eventId;
     }
 
     @PutMapping("/events/{eventId}")
--- src/main/java/com/hyunseo/covidreserve/error/APIExceptionHandler.java
+++ src/main/java/com/hyunseo/covidreserve/error/APIExceptionHandler.java
@@ -3,11 +3,14 @@ package com.hyunseo.covidreserve.error;
 import com.hyunseo.covidreserve.constant.ErrorCode;
 import com.hyunseo.covidreserve.dto.APIErrorResponse;
 import com.hyunseo.covidreserve.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -17,28 +20,59 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice(annotations = RestController.class)
 public class APIExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> general(GeneralException e) {
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
         ErrorCode errorCode = e.getErrorCode();
-        HttpStatus status = errorCode.isClientSideError() ?
+        HttpStatus statusCode = errorCode.isClientSideError() ?
                 HttpStatus.BAD_REQUEST :
                 HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(
-                        false, errorCode, errorCode.getMessage(e)
-                ));
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(
+                        false, errorCode.getCode(), errorCode.getMessage(e)
+                ),
+                HttpHeaders.EMPTY,
+                statusCode,
+                request
+        );
     }
 
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> exception(Exception e) {
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
         ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        HttpStatus statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(
-                        false, errorCode, errorCode.getMessage(e)
-                ));
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(
+                        false, errorCode.getCode(), errorCode.getMessage(e)
+                ),
+                HttpHeaders.EMPTY,
+                statusCode,
+                request
+        );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatusCode statusCode,
+            WebRequest request
+    ) {
+        ErrorCode errorCode = statusCode.is4xxClientError() ?
+                ErrorCode.SPRING_BAD_REQUEST :
+                ErrorCode.SPRING_INTERNAL_ERROR;
+
+        return super.handleExceptionInternal(
+                ex,
+                APIErrorResponse.of(
+                        false, errorCode.getCode(), errorCode.getMessage(ex)
+                ),
+                headers,
+                statusCode,
+                request
+         );
     }
 }

c299079b721e3f21c73169cca5918447e5f021a8
eunoiahyunseo <heart20021010@gmail.com>
2023-01-06 16:23:57 +0900

    Implement #11 - view controller advice
    뷰에 대응하는 `@ControllerAdvice` 구현
    프로젝트 정의 에러와 전체 에러를 잡는 구성

---
+++ src/main/java/com/hyunseo/covidreserve/error/BaseExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.hyunseo.covidreserve.error;
+
+import com.hyunseo.covidreserve.constant.ErrorCode;
+import com.hyunseo.covidreserve.dto.APIErrorResponse;
+import com.hyunseo.covidreserve.exception.GeneralException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+/**
+ * @author ihyeonseo
+ */
+
+@ControllerAdvice
+public class BaseExceptionHandler {
+    @ExceptionHandler
+    public ModelAndView general(GeneralException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(e)
+                ),
+                status);
+    }
+
+    @ExceptionHandler
+    public ModelAndView exception (Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(e)
+                ),
+                status);
+    }
+}

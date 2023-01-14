5cc6acf506d198db107fc1eccef568586375be98
eunoiahyunseo <heart20021010@gmail.com>
2023-01-14 13:58:11 +0900

    Refactor #17 - ThymeleafConfig & properties and test
    Thymeleaf view template를 배우고 코드 짜기 전에 코드 리팩토링 및 사전 준비 작업

--- src/main/java/com/hyunseo/covidreserve/controller/EventController.java
+++ src/main/java/com/hyunseo/covidreserve/controller/EventController.java
@@ -1,6 +1,7 @@
 package com.hyunseo.covidreserve.controller;
 
 import com.hyunseo.covidreserve.constant.EventStatus;
+import com.hyunseo.covidreserve.dto.EventResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,7 +21,7 @@ import java.util.Map;
 @Controller
 public class EventController {
 
-    @GetMapping("/")
+    @GetMapping
     public ModelAndView events(
             Integer placeId,
             String eventName,
@@ -28,18 +30,46 @@ public class EventController {
             LocalDateTime eventEndDatetime
     ) {
         Map<String, Object> map = new HashMap<>();
-        map.put("placeName", "place-" + placeId);
-        map.put("eventName", eventName);
-        map.put("eventStatus", eventStatus);
-        map.put("eventStartDatetime", eventStartDatetime);
-        map.put("eventEndDatetime", eventEndDatetime);
+        map.put("events", List.of(EventResponse.of(
+                        1L,
+                        "오후 운동",
+                        EventStatus.OPENED,
+                        LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                        LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                        0,
+                        24,
+                        "마스크 꼭 착용하세요"
+                ), EventResponse.of(
+                        2L,
+                        "오후 운동",
+                        EventStatus.OPENED,
+                        LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                        LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                        0,
+                        24,
+                        "마스크 꼭 착용하세요"
+                )
+        ));
 
-        return new ModelAndView("admin/events", map);
+        return new ModelAndView("event/index", map);
     }
 
     @GetMapping("/{eventId}")
-    public String eventDetail(@PathVariable Integer eventId) {
-        return "event/detail";
-    }
+    public ModelAndView eventDetail(@PathVariable Long eventId) {
+        Map<String, Object> map = new HashMap<>();
 
+        // TODO: 임시 데이터. 추후 삭제 예정
+        map.put("event", EventResponse.of(
+                eventId,
+                "오후 운동",
+                EventStatus.OPENED,
+                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
+                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
+                0,
+                24,
+                "마스크 꼭 착용하세요"
+        ));
+
+        return new ModelAndView("event/detail", map);
+    }
 }
